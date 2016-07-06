a8c02e4d21ab8f6b6c69d04e1111ddcb395b76d0
Reuben Brandt <Reubennbrandt@gmail.com>
2016-07-06 11:38:38 -0700

    Add playing area div

--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,66 @@
+// Notes before I begin
+// The board will be built using a static image.
+// There will be a counter in the upper right hand corner
+// The cursor will be shaped like a hammer
+// There will be 6 different holes which will always be in the same place.
+// At a RANDOM interval between 0 and 1 second, a mole will appear out of one of the Holes.
+// If the user hits the mole it will:
+   // change it's face to look hurt
+   // Increment the counter
+// If the User doesn't hit a mole after 0.5 seconds (timing may change)
+  // The counter will not increment
+  // The face will not change
+  // The the mole will disappear.
+
+var Mole = React.createClass({
+  // getInitialState: function(){
+  //   return {hit: false}
+  // },
+  hit: function(){
+    alert('you hit the mole');
+  },
+  render: function(){
+    return (
+      <div onClick={this.hit} className="mole"></div>
+    );
+  },
+});
+
+var Surface = React.createClass({
+  render: function(){
+    return (<div className = "surface">
+      {this.state.mole.map(function(mole,i){
+        return(
+          <Mole key={i}></Mole>
+        );
+      })}
+    </div>)
+  },
+  getInitialState: function(){
+    return{
+      mole: ['1']
+    };
+  }
+})
+
+var Board = React.createClass({
+  getInitialState: function(){
+    return{
+      mole:[
+      '1'
+      ]
+    };
+  },
+  render: function(){
+    return (<div className='board'> <Surface> </Surface>
+    </div>
+    );
+  }
+})
 
 
-React.render(<Board count={1}/>,
+
+React.render(<Board></Board>,
     document.getElementById('react-container'));
 
 
